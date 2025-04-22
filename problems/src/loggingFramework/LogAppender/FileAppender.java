793a48b260318a38f115bbc8badc2b2691ea6446
Pulasthya <pulasthyareddy@gmail.com>
2025-04-22 18:56:33 -0400

    fixing new line issue

--- problems/src/loggingFramework/LogAppender/FileAppender.java
+++ problems/src/loggingFramework/LogAppender/FileAppender.java
@@ -14,7 +14,7 @@ public class FileAppender implements LogAppender {
     @Override
     public void append(LogMessage message){
         try (FileWriter writer = new FileWriter(filePath, true)) {
-            writer.write(message.toString());
+            writer.write(message.toString() + "\n");
             System.out.println("Content successfully added to log file");
         }
         catch (IOException e){
